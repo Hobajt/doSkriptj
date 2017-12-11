b83bcba3f0400e5f2d4a54def5b4d3cb23e586ef
Hobajt <kopecki111@gmail.com>
2017-12-11 01:44:26 +0100

    Added additional views and templates

--- projektis/restaurace/urls.py
+++ projektis/restaurace/urls.py
@@ -6,4 +6,7 @@ app_name='restaurace'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id_restaurant>/', views.resInfo, name='resInfo'),
+    path('dishes/', views.dishIndex, name='dishIndex'),
+    path('dishes/<int:id_dish>/', views.dishInfo, name='dishInfo'),
+    path('dishes/byType/<int:id_dishType>/', views.dishByType, name='dishByType'),
 ]
--- projektis/restaurace/views.py
+++ projektis/restaurace/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
-from .models import Restaurant, Review
+from .models import Restaurant, Review, Dish, DishType
 
 def index(request):
-    return render(request, 'restaurace/index.html')
+    restaurants = Restaurant.objects.all().order_by("name")
+    return render(request, 'restaurace/index.html', {'restaurants': restaurants})
 
 def resInfo(request, id_restaurant):
     restaurant= get_object_or_404(Restaurant, pk=id_restaurant)
     return render(request, 'restaurace/resInfo.html', {'restaurant': restaurant})
+
+def dishIndex(request):
+    dishes= Dish.objects.all().order_by("name")
+    return render(request, 'restaurace/dishIndex.html', {'dishes': dishes})
+
+def dishInfo(request, id_dish):
+    dish= get_object_or_404(Dish, pk=id_dish)
+    return render(request, 'restaurace/dishInfo.html', {'dish': dish})
+
+def dishByType(request, id_dishType):
+    dishType= get_object_or_404(DishType, pk=id_dishType)
+    return render(request, 'restaurace/dishByType.html', {'dishType': dishType})
+
 
 
